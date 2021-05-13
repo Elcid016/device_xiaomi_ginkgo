69019de9120b0101d8c77f1dd0d62ffea5a6cd86
Elcid016 <45820772+Elcid016@users.noreply.github.com>
2021-05-13 12:32:57 -0300

    Update AndroidProducts.mk

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -1,12 +1,12 @@
 #
-# Copyright (C) 2020 The  DotOS Project
+# Copyright (C) 2020 The PalladiumOS Project
 #
 # SPDX-License-Identifier: Apache-2.0
 #
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/dot_ginkgo.mk
+    $(LOCAL_DIR)/palladium_ginkgo.mk
 
 COMMON_LUNCH_CHOICES := \
-    dot_ginkgo-user \
-    dot_ginkgo-userdebug \
-    dot_ginkgo-eng
+    palladium_ginkgo-user \
+    palladium_ginkgo-userdebug \
+    palladium_ginkgo-eng
